a33a2944c32c8067e04a15c65c161e8707f99fc9
Ollie Rattue <orattue@toomanytabs.com>
2011-03-04 15:51:41 +0100

    CI 2.0 Reactor modification - Updated Parent Constructor calls of model and removed config table loading which was left from an unrelated previous project. Added note to README about CI version and functionality status

--- models/autoresponder_model.php
+++ models/autoresponder_model.php
@@ -1,15 +1,11 @@
 <?php
 
-class Autoresponder_model extends Model { 
-
-	// Protected or private properties
-	protected $_table;
-	
+class Autoresponder_model extends CI_Model
+{
 	// Constructor
 	public function __construct()
 	{
-		parent::Model();
-		$this->_table = $this->config->item('database_tables');
+		parent::__construct();
 	}
 	
 	// --------------------------------------------------------------------
