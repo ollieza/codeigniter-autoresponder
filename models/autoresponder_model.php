9300659a808a09bd125faabee578feb9952ce62d
Ollie Rattue <orattue@toomanytabs.com>
2011-05-20 19:56:52 +0100

    Removed table variable which I use on other projects but is appropriate for this

--- models/autoresponder_model.php
+++ models/autoresponder_model.php
@@ -13,16 +13,16 @@ class Autoresponder_model extends CI_Model
 	function get($autoresponder_name = NULL) 
 	{
 		$this->db->select('*');
-       	$this->db->from($this->_table['autoresponders']);
-      	$this->db->where('autoresponder_name', $autoresponder_name);
-       	return $this->db->get();   
+       		$this->db->from('autoresponders');
+      		$this->db->where('autoresponder_name', $autoresponder_name);
+       		return $this->db->get();   
 	}
 	
 	// --------------------------------------------------------------------
 	
 	function log_email($data = array())
 	{
-        $this->db->insert($this->_table['autoresponder_log'], $data);
+        	$this->db->insert('autoresponder_log', $data);
  
 		if ($this->db->affected_rows() == '1')
 		{
@@ -30,7 +30,7 @@ class Autoresponder_model extends CI_Model
 		}
  
 		return FALSE;
-    }
+    	}
 	
 	// --------------------------------------------------------------------
 }
