665e88a715a92d1917745d018f454377e624e533
ollieza <288799+ollieza@users.noreply.github.com>
2022-04-04 16:11:51 +0200

    Update autoresponder.php

--- libraries/autoresponder.php
+++ libraries/autoresponder.php
@@ -5,11 +5,7 @@
  * Sends emails using templates stored in database
  *
  * @package   autoresponder
- * @version   1.0
- * @author    Ollie Rattue, Too many tabs <orattue[at]toomanytabs.com>
- * @copyright Copyright (c) 2010, Ollie Rattue
  * @license   http://www.opensource.org/licenses/mit-license.php
- * @link      http://github.com/ollierattue/codeigniter-autoresponder
  */
 
 class Autoresponder {
